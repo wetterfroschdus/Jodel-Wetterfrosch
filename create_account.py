473f66fa4a24b4e97bd55114a03aa74d22cbdd9d
wetterfroschdus <wetterfroschdus@gmail.com>
2017-07-14 18:10:51 +0200

    Added DWD and fixed to work with latest jodel_api
    Now uses gcm and also writes more data to the account.json.

--- create_account.py
+++ create_account.py
@@ -3,9 +3,10 @@ import jodel_api
 import requests
 from operator import itemgetter
 from builtins import input
+from ftplib import FTP
 
 class InputJodelAcc(object):
-    def __init__(self, lat=None, lng=None, city=None, access_token=None, expiration_date=None, refresh_token=None, distinct_id=None, device_uid=None):
+    def __init__(self, lat=None, lng=None, city=None, access_token=None, expiration_date=None, refresh_token=None, distinct_id=None, device_uid=None, legacy=None):
         self.access_token = access_token
         self.expiration_date = expiration_date
         self.refresh_token = refresh_token
@@ -14,11 +15,14 @@ class InputJodelAcc(object):
         self.lat = lat
         self.lng = lng
         self.city = city
+        self.legacy = legacy
 
 class InputWeather(object):
-    def __init__(self, API_KEY, CITY_code):
+    def __init__(self, API_KEY, CITY_code, dwdname, dwdpass):
         self.API_KEY = API_KEY
         self.CITY = CITY_code
+        self.dwdname = dwdname
+        self.dwdpass = dwdpass
 
 def y_n(question):
     answer = ""
@@ -40,8 +44,8 @@ def write_data(file_data):
         json.dump(file_data, outfile, indent=4)
 
 def create_data():
-    data_weather = get_data_Weather()
     data_jodel = get_data_Jodel()
+    data_weather = get_data_Weather()
     file_data = {
         "lat":data_jodel.lat,
         "lng":data_jodel.lng,
@@ -52,27 +56,38 @@ def create_data():
         "distinct_id":data_jodel.distinct_id,
         "refresh_token":data_jodel.refresh_token,
         "device_uid":data_jodel.device_uid,
-        "access_token":data_jodel.access_token}
+        "access_token":data_jodel.access_token,
+        "legacy":data_jodel.legacy,
+        "dwdname":data_weather.dwdname,
+        "dwdpass":data_weather.dwdpass
+        }
     return file_data
 
 def get_data_Jodel():
     Jodels_loc = get_Jodels_loc()
     if y_n("Do you already have data for a valid Jodel account?"):
         print("Input the data for your Jodel account:\n")
+        legacy = y_n("Is the account a legacy (pre Jodel 4.48) account?")
         access_token = input("Access Token:\n")
         expiration_date = int(input("Expiration Date:\n"))
         refresh_token = input("Refresh Token:\n")
         distinct_id = input("Distinct ID:\n")
         device_uid = input("Device UID:\n")
-        return InputJodelAcc(access_token=access_token, expiration_date=expiration_date, refresh_token=refresh_token, distinct_id=distinct_id, device_uid=device_uid,lat=Jodels_loc.lat,lng=Jodels_loc.lng,city=Jodels_loc.city)
+        return InputJodelAcc(access_token=access_token, expiration_date=expiration_date, refresh_token=refresh_token, distinct_id=distinct_id, device_uid=device_uid,lat=Jodels_loc.lat,lng=Jodels_loc.lng,city=Jodels_loc.city, legacy=legacy)
     Jodel_account = create_account(Jodels_loc.lat, Jodels_loc.lng, Jodels_loc.city)
-    return InputJodelAcc(access_token=Jodel_account.access_token, expiration_date=Jodel_account.expiration_date, refresh_token=Jodel_account.refresh_token, distinct_id=Jodel_account.distinct_id, device_uid=Jodel_account.device_uid,lat=Jodels_loc.lat,lng=Jodels_loc.lng,city=Jodels_loc.city)
+    return InputJodelAcc(access_token=Jodel_account.access_token, expiration_date=Jodel_account.expiration_date, refresh_token=Jodel_account.refresh_token, distinct_id=Jodel_account.distinct_id, device_uid=Jodel_account.device_uid,lat=Jodels_loc.lat,lng=Jodels_loc.lng,city=Jodels_loc.city, legacy=Jodel_account.legacy)
 
 def create_account(lat,lng,city):
     account = jodel_api.JodelAccount(lat=lat, lng=lng, city=city)
-    account.verify_account()
-    account.get_account_data()
-    return InputJodelAcc(access_token=account.access_token,expiration_date=account.expiration_date,refresh_token=account.refresh_token,distinct_id=account.distinct_id,device_uid=account.device_uid)
+    while True:
+        try:
+            a = jodel_api.AndroidAccount()
+            account.verify(a)
+            account.get_account_data()
+            break
+        except:
+            raise Exception("Couldn't verify account. Please retry in a few Minutes.")
+    return InputJodelAcc(access_token=account.access_token,expiration_date=account.expiration_date,refresh_token=account.refresh_token,distinct_id=account.distinct_id,device_uid=account.device_uid, legacy=False)
 
 def get_Jodels_loc():
     print("Input the location your Jodels should be posted at:\n")
@@ -83,7 +98,7 @@ def get_Jodels_loc():
 
 def get_data_Weather():
     while True:
-        print("Input the data for the weather API.\n")
+        print("Input the data for the Wunderground weather API.\n")
         API_KEY = input("API Key:\n")
         COUNTRY = input("Country:\n")
         CITY = input("City:\n")
@@ -92,8 +107,17 @@ def get_data_Weather():
             if y_n("Retry?") == False:
                 raise Exception("User abort on Weather Data select.")
         else:
-            break                
-    return InputWeather(API_KEY, CITY_code)
+            break
+    while True:
+        print("Input the data for the DWD FTP Server.\n")
+        dwdname = input("Username:\n")
+        dwdpass = input("Password:\n")
+        if not check_dwdftp(dwdname, dwdpass):        
+            if y_n("Retry?") == False:
+                raise Exception("User abort on DWD FTP Server data input.")
+        else:
+            break             
+    return InputWeather(API_KEY, CITY_code,dwdname,dwdpass)
 
 def check_weather(API_KEY, COUNTRY, CITY):
     response = requests.get("https://api.wunderground.com/api/%s/geolookup/q/%s/%s.json" % (API_KEY,COUNTRY,CITY))
@@ -121,6 +145,15 @@ def check_weather(API_KEY, COUNTRY, CITY):
         return CITY_code
       
     raise Exception("Got invalid data from Weather Provider.\nData:%s" % response_json)
+
+def check_dwdftp(dwdname, dwdpass):
+    try:
+        ftp = FTP('ftp-outgoing2.dwd.de')
+        ftp.login(dwdname, dwdpass)
+        return True
+    except:
+        print("Invalid username or password")
+        return False
 
 write_data(create_data())
 print('Data has been successfully written. You can now use "jodel_wetterfrosch.py"')
